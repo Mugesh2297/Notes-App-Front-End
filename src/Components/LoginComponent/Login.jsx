8add77f88b85e0611d683cab7d22b6732ee89d9a
Mugesh <mugeshkumark22@gmail.com>
2022-12-19 11:36:05 +0530

    login backend added functions

--- src/Components/LoginComponent/Login.jsx
+++ src/Components/LoginComponent/Login.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./login.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
-import {  NavLink } from 'react-router-dom';
+import {  NavLink, useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 
 function Login() {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const formik = useFormik({
+      initialValues: {
+          email: "",
+          password: ""
+      },
+      validate: (values) => {
+          let errors = {};
+          if (values.email === "") {
+              errors.email = "Please Enter Email Id"
+          }
+          if (values.password === "") {
+              errors.password = "Please Enter Password"
+          }
+
+          return errors;
+      },
+
+      onSubmit: async (values) => {
+          try {
+              var response = await axios.post("http://localhost:3001/register/signin", values);
+              console.log(response)
+              localStorage.setItem("token", response.data);
+              navigate("/home");
+
+
+          } catch (err) {
+              console.log(err.response);
+              console.log(err.response.data.msg);
+             if(err.response.data.code === "email"){
+              setValue(err.response.data.msg)
+             }
+             else if (err.response.data.code === "password"){
+              setValue(err.response.data.msg)
+             }
+             else if (err.response.data.code === "user"){
+              Swal.fire({ title: err.response.data.msg,  icon: 'error', confirmButtonText: 'okay'});
+             }
+          }
+      }
+  })
   return (
+    <div className='head'>
     <div>
       <section className='login py-5 main'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12 text-center'>
@@ -16,15 +62,20 @@ function Login() {
           <div className='row '>
             <div className=' col-lg-12 col-md-12 col-sm-12 col-12 text-center py-5 '>
               <h1 className='animate__animated animate__heartBeat loginTitle'>Welcome Back</h1>
-              <form>
+              <form onSubmit={formik.handleSubmit}>
                 <div className='form-row  pt-3 text-center'>
                   <div className='offset-1 col-lg-10'>
-                    <input type="email" className='inp my-3 px-3' placeholder='email'></input>
+                    <input type="email" className='inp my-3 px-3' placeholder='email'
+                     value={formik.values.email} onChange={formik.handleChange} name="email"></input>
+                     <div><span className="valueError"  style={{ color: 'red' }}>{formik.errors.email}</span></div>
                   </div>
                 </div>
                 <div className='form-row pt-3 text-center'>
                   <div className='offset-1 col-lg-10'>
-                    <input type="password" className='inp px-3' placeholder='password'></input>
+                    <input type="password" className='inp px-3' placeholder='password'
+                     value={formik.values.password} onChange={formik.handleChange} name="password"></input>
+                     <div className="passwordError" ><span  style={{ color: 'red' }}>{formik.errors.password}</span></div>
+                     <div><span  style={{ color: 'red' }}>{value}</span></div>
                   </div>
                 </div>
                 <div className='form-row py-4' >
@@ -41,6 +92,7 @@ function Login() {
           </div>
         </div>
       </section>
+    </div>
     </div>
   )
 }
