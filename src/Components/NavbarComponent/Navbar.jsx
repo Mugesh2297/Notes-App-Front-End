50b781e4210e233af36261944f2f03e3aff984b3
Mugesh <mugeshkumark22@gmail.com>
2023-01-18 20:37:02 +0530

    navbar changes

--- src/Components/NavbarComponent/Navbar.jsx
+++ src/Components/NavbarComponent/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
-import "./navbar.css"
+import "./navbar.css";
+import 'animate.css';
 import { Link } from "react-router-dom"
 import { faBars,  faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,13 +8,14 @@ const Navbar = () => {
   const [Mobile, setMobile] = useState(false)
   return (
     <>
+    <div className="homePage">
       <nav className='navbar'>
-        <h3 className='logo'>Notes App</h3>
+        <h3 className='logo headfont animate__animated animate__heartBeat'>Notes App</h3>
         {/*
         if large screen ma xa bhane Mobile add huxa
         if mobile screen ma xa bhane nav-links-mobile add huxa
         */}
-        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
+        <ul className= {Mobile ? "nav-links-mobile" : "nav-links animate__animated animate__heartBeat"} onClick={() => setMobile(false)}>
           <Link to='/' className='home'>
             <li>Home</li>
           </Link>
@@ -32,6 +34,7 @@ const Navbar = () => {
           {Mobile ?  <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon> :  <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>}
         </button>
       </nav>
+      </div>
     </>
   )
 }
